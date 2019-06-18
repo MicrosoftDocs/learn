90dd71ce7a8e2114c7f2242bb8143fc88138ea0e
Jason Boeshart <jboeshart@users.noreply.github.com>
2019-06-18 10:17:38 -0500

    Update setup.sh

--- learn-pr/azure/develop-app-that-queries-azure-sql/code/setup.sh
+++ learn-pr/azure/develop-app-that-queries-azure-sql/code/setup.sh
@@ -8,10 +8,10 @@ az webapp deployment user set --user-name $GIT_USERNAME --password $GIT_PASSWORD
 
 az appservice plan create --name educationAppServicePlan$ID --resource-group sqlrg --sku B1 --is-linux
 
-az webapp create --resource-group sqlrg --plan educationAppServicePlan$ID --name educationapp$ID --runtime "DOTNETCORE|2.1" --deployment-local-git
+az webapp create --resource-group sqlrg --plan educationAppServicePlan$ID --name educationapp-$ID --runtime "DOTNETCORE|2.1" --deployment-local-git
 
-git remote add azure https://$GIT_USERNAME@educationapp$ID.scm.azurewebsites.net/educationapp$ID.git
+git remote add azure https://$GIT_USERNAME@educationapp-$ID.scm.azurewebsites.net/educationapp-$ID.git
 
-git push https://$GIT_USERNAME:$GIT_PASSWORD@educationapp$ID.scm.azurewebsites.net/educationapp$ID.git
+git push https://$GIT_USERNAME:$GIT_PASSWORD@educationapp-$ID.scm.azurewebsites.net/educationapp-$ID.git
 
-echo "Web app deployed to https://educationapp$ID.azurewebsites.net/"
+echo "Web app deployed to https://educationapp-$ID.azurewebsites.net/"
