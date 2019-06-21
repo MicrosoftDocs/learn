1cd3d476f1b1ea6843b6c6043ec7e84762c1f359
Thomas Petchel <tpetchel@gmail.com>
2019-06-21 00:33:10 -0700

    Update procedures
    ...to match reality :/

--- learn-pr/azure-devops/host-build-agent/includes/code/build-tools.sh
+++ learn-pr/azure-devops/host-build-agent/includes/code/build-tools.sh
@@ -15,6 +15,10 @@ apt-get install -y nodejs
 # Install gulp
 npm install -g gulp
 
+# Change ownership of npm directories to the sudo (non-root) user
+chown -R $SUDO_USER ~/.npm
+chown -R $SUDO_USER ~/.config
+
 # Install .NET as the sudo (non-root) user
 sudo -i -u $SUDO_USER bash << EOF
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -c Current -v $DOTNET_VERSION
