1c0700a124d2d3ead3fd66397db8e2c4683ce9c5
Scott Addie <scott.addie@microsoft.com>
2021-01-27 13:31:18 -0600

    Code file update

--- learn-pr/aspnetcore/microservices-data-aspnet-core/code/src/services/basket/basket-api/startup.cs
+++ learn-pr/aspnetcore/microservices-data-aspnet-core/code/src/services/basket/basket-api/startup.cs
@@ -9,23 +9,7 @@ public static IServiceCollection AddCustomHealthCheck(
                  name: "redis-check",
                  tags: new string[] { "redis" });
 
-    if (configuration.GetValue<bool>("AzureServiceBusEnabled"))
-    {
-        hcBuilder
-            .AddAzureServiceBusTopic(
-                configuration["EventBusConnection"],
-                topicName: "eshop_event_bus",
-                name: "basket-servicebus-check",
-                tags: new string[] { "servicebus" });
-    }
-    else
-    {
-        hcBuilder
-            .AddRabbitMQ(
-                $"amqp://{configuration["EventBusConnection"]}",
-                name: "basket-rabbitmqbus-check",
-                tags: new string[] { "rabbitmqbus" });
-    }
+    // code omitted for brevity
 
     return services;
 }
