937bbbd3da702068c8cffb4822b9b3c936d32e62
António Sérgio Azevedo <22173500+asergaz@users.noreply.github.com>
2023-02-27 03:09:09 +0000

    unit 5 and adding code

--- learn-pr/iot/monitor-and-manage-device-with-iot-central/includes/5-define-coffee-machine-device-simulator.md
+++ learn-pr/iot/monitor-and-manage-device-with-iot-central/includes/5-define-coffee-machine-device-simulator.md
@@ -1,7 +1,62 @@
 <!-- Follow this: https://learn.microsoft.com/azure/iot-central/core/concepts-device-templates#device-models
+And this: https://learn.microsoft.com/azure/iot-central/core/concepts-device-implementation
 
 TODO: Add Node App code here!? -->
 
-The device developer must make sure that the device implements the behaviors defined in the device model so that IoT Central can monitor and manage the device. 
+The device developer must make sure that the device implements the behaviors defined in the device model so that Azure IoT Central can monitor and manage the device.
 
-Here, you'll see ...
+In the IoT enabled coffee machines example you'll simulate a device to validate your scenario before connecting a physical device.
+
+Here, you'll see how a device developer uses the device model defined in the *CoffeeMaker.json*, to write a Node.js application that will simulate a coffee machine.
+
+## IoT Plug and Play conventions
+
+Devices that connect to IoT Central should follow the [IoT Plug and Play](/azure/iot-develop/overview-iot-plug-and-play) set of [conventions](/azure/iot-develop/concepts-convention). One of these conventions is that a device should send the [Digital Twin Model Identifier (DTMI)](https://github.com/Azure/opendigitaltwins-dtdl/blob/master/DTDL/v2/DTDL.v2.md#digital-twin-model-identifier) when it connects. The DTMI enables the IoT Central application to assign the device to the correct device template.
+
+The [Azure IoT device SDKs](/azure/iot-hub/iot-hub-devguide-sdks) include support for the IoT Plug and Play conventions.
+
+## Review the code
+
+<!-- TODO: Can I download a file in a Module? -->
+You can open the *CoffeeMaker.js* file in a text editor to see the entire code.
+
+The `provisioningClient` object:
+
+- Sets the `dtmi:com:example:ConnectedCoffeeMaker;1` model ID before it registers the device.
+- Opens the connection in the `centralClient` object.
+
+<!-- TODO: Reference code to provisioningClient object -->
+
+The `connectCallback` method:
+
+- Starts a loop to send temperature and humidity telemetry, whether it's currently brewing and when a cup is detected every 1 second.
+- Creates command handlers for two commands: `SetMaintenanceMode` and `StartBrewing`.
+- Sends device properties via the device twin `DeviceWarrantyExpired` *read-only* property.
+- Creates a *writable* property handler - `handleSettings`,  to set the optimal water temperature that comes from IoT Central via the device twin.
+
+<!-- TODO: reference code to connectCallback method -->
+
+### Telemetry
+
+The function `sendTelemetry` sends telemetry in the format that the device model specifies.
+
+<!-- TODO: reference code to sendTelemetry function -->
+
+### Properties
+
+The model specifies the property names and data types to synchronize property values between the device and IoT Central via the device twin. The function `sendDeviceProperties` sends the `warrantyState` of the device and the function `handleSettings` handle `OptimalTemperature` changes that come from IoT Central.
+
+<!-- TODO: reference code to sendDeviceProperties and handleSettings functions -->
+
+### Commands
+
+The model specifies the command names and parameters that the device should use. The functions `onCommandMaintenance` and `onCommandStartBrewing` handle the commands sent from IoT Central.
+
+<!-- TODO: reference code to onCommandMaintenance and onCommandStartBrewing functions -->
+
+### Cloud properties and Views
+
+Cloud properties and Views don't affect the code that a device developer writes to implement the device model.
+
+## Coffee Maker Node.js application
+<!-- TODO: I can remove this section if the code can be downloaded  -->
---
+++ learn-pr/iot/monitor-and-manage-device-with-iot-central/resources/CoffeeMaker.js
@@ -0,0 +1,256 @@
+// Use the Azure IoT device SDK for devices that connect to Azure IoT Central.
+var iotHubTransport = require('azure-iot-device-mqtt').Mqtt;
+var Client = require('azure-iot-device').Client;
+var Message = require('azure-iot-device').Message;
+var ProvisioningTransport = require('azure-iot-provisioning-device-mqtt').Mqtt;
+var SymmetricKeySecurityClient = require('azure-iot-security-symmetric-key').SymmetricKeySecurityClient;
+var ProvisioningDeviceClient = require('azure-iot-provisioning-device').ProvisioningDeviceClient;
+
+// DPS connection information
+var modelIdObject = { modelId: 'dtmi:com:example:ConnectedCoffeeMaker;1' };
+var provisioningHost = 'global.azure-devices-provisioning.net';
+var idScope = '{ID scope}';
+var registrationId = '{Device ID}';
+var symmetricKey = '{Primary key}';
+var provisioningSecurityClient = new SymmetricKeySecurityClient(registrationId, symmetricKey);
+var provisioningClient = ProvisioningDeviceClient.create(provisioningHost, idScope, new ProvisioningTransport(), provisioningSecurityClient);
+var centralClient;
+
+// Global variables
+var optimalTemperature = 96;
+var cupState = 'detected';
+var brewingState = 'notbrewing';
+var cupTimer = 20;
+var brewingTimer = 0;
+var maintenanceState = false;
+var warrantyState = Math.random() < 0.5 ? true : false;
+
+// Helper function to produce nice numbers (##.#)
+function niceNumber(value) {
+    var number = (Math.round(value * 10.0)).toString();
+    return number.substr(0, 2) + '.' + number.substr(2, 1);
+}
+
+// Send device simulated telemetry measurements
+function sendTelemetry() {
+    // Simulate the telemetry values
+    var temperature = optimalTemperature + (Math.random() * 4) - 2;
+    var humidity = 20 + (Math.random() * 80);
+
+    // Cup timer - every 20 seconds randomly decide if the cup is present or not
+    cupTimer--;
+
+    if (cupTimer == 0) {
+        cupTimer = 20;
+        cupState = Math.random() < 0.5 ? 'detected' : 'notdetected';
+    }
+
+    // Brewing timer
+    if (brewingTimer > 0) {
+        brewingTimer--;
+
+        // Finished brewing
+        if (brewingTimer == 0) {
+            brewingState = 'notbrewing';
+        }
+    }
+
+    // Create the data JSON package
+    var data = JSON.stringify(
+        {
+            WaterTemperature: temperature,
+            AirHumidity: humidity,
+            CupDetected: cupState,
+            Brewing: brewingState
+        });
+
+    // Create the message with the above defined data
+    var message = new Message(data);
+
+    // Show the information in console
+    var infoTemperature = niceNumber(temperature);
+    var infoHumidity = niceNumber(humidity);
+    var infoCup = (cupState == 'detected') ? 'Y' : 'N';
+    var infoBrewing = (brewingState == 'brewing') ? 'Y' : 'N';
+    var infoMaintenance = maintenanceState ? 'Y' : 'N';
+
+    console.log('Telemetry send: Temperature: ' + infoTemperature +
+        ' Humidity: ' + infoHumidity + '%' +
+        ' Cup Detected: ' + infoCup +
+        ' Brewing: ' + infoBrewing +
+        ' Maintenance Mode: ' + infoMaintenance);
+
+    // Send the message
+    centralClient.sendEvent(message, function (errorMessage) {
+        // Error
+        if (errorMessage) {
+            console.log('Failed to send message to Azure IoT Hub: ${err.toString()}');
+        }
+    });
+}
+
+// Send device properties
+function sendDeviceProperties(deviceTwin) {
+    var properties =
+    {
+        DeviceWarrantyExpired: warrantyState
+    };
+
+    console.log(' * Property - Warranty State: ' + warrantyState.toString());
+
+    deviceTwin.properties.reported.update(properties, (errorMessage) =>
+        console.log(` * Sent device properties ` + (errorMessage ? `Error: ${errorMessage.toString()}` : `(success)`)));
+}
+
+// Optimal temperature setting
+var settings =
+{
+    'OptimalTemperature': (newValue, callback) => {
+        setTimeout(() => {
+            optimalTemperature = newValue;
+            callback(optimalTemperature, 200, "Successfully updated OptimalTemperature");
+        }, 1000);
+    }
+};
+
+// Handle settings changes that come from Azure IoT Central via the device twin.
+function handleSettings(deviceTwin) {
+    deviceTwin.on('properties.desired', function (desiredChange) {
+        // Iterate all settings looking for the defined one
+        for (let setting in desiredChange) {
+            // Setting we defined
+            if (settings[setting]) {
+                // Console info
+                console.log(` * Received setting: ${setting}: ${desiredChange[setting]}`);
+
+                // Update 
+                settings[setting](desiredChange[setting], (newValue, status, message) => {
+                    var patch =
+                    {
+                        [setting]:
+                        {
+                            value: newValue,
+                            ac: status,
+                            av: desiredChange.$version,
+                            ad: message
+                        }
+                    }
+                    deviceTwin.properties.reported.update(patch, (err) => console.log(` * Sent setting update for ${setting} ` +
+                        (err ? `error: ${err.toString()}` : `(success)`)));
+                });
+            }
+        }
+    });
+}
+
+// Maintenance mode command
+function onCommandMaintenance(request, response) {
+    // Display console info
+    console.log(' * Maintenance command received');
+
+    // Console warning
+    if (maintenanceState) {
+        console.log(' - Warning: The device is already in the maintenance mode.');
+    }
+
+    // Set state
+    maintenanceState = true;
+
+    // Respond
+    response.send(200, 'Success', function (errorMessage) {
+        // Failure
+        if (errorMessage) {
+            console.error('[IoT hub Client] Failed sending a method response:\n' + errorMessage.message);
+        }
+    });
+}
+
+// Start Brewing command
+function onCommandStartBrewing(request, response) {
+    // Display console info
+    console.log(' * Brewing command received');
+
+    // Console warning
+    if (brewingState == 'brewing') {
+        console.log(' - Warning: The device is already brewing.');
+    }
+
+    if (cupState == 'notdetected') {
+        console.log(' - Warning: The cup has not been detected.');
+    }
+
+    if (maintenanceState == true) {
+        console.log(' - Warning: The device is in maintenance state.');
+    }
+
+    // Set state - brew for 30 seconds
+    if ((cupState == 'detected') && (brewingState == 'notbrewing') && (maintenanceState == false)) {
+        brewingState = 'brewing';
+        brewingTimer = 30;
+    }
+
+    // Respond
+    response.send(200, 'Success', function (errorMessage) {
+        // Failure
+        if (errorMessage) {
+            console.error('[IoT hub Client] Failed sending a method response:\n' + errorMessage.message);
+        }
+    });
+}
+
+// Handle device connection to Azure IoT Central
+var connectCallback = (errorMessage) => {
+    // Connection error
+    if (errorMessage) {
+        console.log(`Device could not connect to Azure IoT Central: ${errorMessage.toString()}`);
+    }
+    // Successfully connected
+    else {
+        // Notify the user
+        console.log('Device successfully connected to Azure IoT Central');
+
+        // Send telemetry measurements to Azure IoT Central every 1 second.
+        setInterval(sendTelemetry, 1000);
+
+        // Set up device command callbacks
+        centralClient.onDeviceMethod('SetMaintenanceMode', onCommandMaintenance);
+        centralClient.onDeviceMethod('StartBrewing', onCommandStartBrewing);
+
+        // Get device twin from Azure IoT Central
+        centralClient.getTwin((errorMessage, deviceTwin) => {
+            // Failed to retrieve device twin
+            if (errorMessage) {
+                console.log(`Error getting device twin: ${errorMessage.toString()}`);
+            }
+            // Success
+            else {
+                // Notify the user
+                console.log('Device Twin successfully retrieved from Azure IoT Central');
+
+                // Send device properties once on device startup
+                sendDeviceProperties(deviceTwin);
+
+                // Apply device settings and handle changes to device settings
+                handleSettings(deviceTwin);
+            }
+        });
+    }
+};
+
+// Start the device (register and connect to Azure IoT Central).
+provisioningClient.setProvisioningPayload(modelIdObject);
+provisioningClient.register((err, result) => {
+    if (err) {
+        console.log('Error registering device: ' + err);
+    } else {
+        console.log('Registration succeeded');
+        console.log('Assigned hub=' + result.assignedHub);
+        console.log('DeviceId=' + result.deviceId);
+        console.log('Payload=' + JSON.stringify(result.payload));
+        var connectionString = 'HostName=' + result.assignedHub + ';DeviceId=' + result.deviceId + ';SharedAccessKey=' + symmetricKey;
+        centralClient = Client.fromConnectionString(connectionString, iotHubTransport);
+        
+        centralClient.setOptions(modelIdObject);
+        centralClient.open(connectCallback);
+    }
+});
